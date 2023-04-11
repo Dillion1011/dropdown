01059a36de01f1dd8285b030927ff70b2d76c807
Dillion1011 <122704037+Dillion1011@users.noreply.github.com>
2023-04-11 07:12:59 -0700

    Add files via upload
    great!!

---
+++ images/dropdown.js
@@ -0,0 +1,56 @@
+
+function hello() {
+    var z = document.getElementById("hello");
+    if(z.style.display=="none"){
+      z.style.display = "block";
+    }
+    else {
+      z.style.display = "none";
+    }
+  }
+  function hey(){
+    var w=document.getElementById("hey");
+    if(w.style.display=="none"){
+      w.style.display ="block";
+    }
+    else{
+      w.style.display = "none";
+    }
+  }
+  function menu(){
+    var b=document.getElementById("men");
+    if(b.style.display=="none"){
+      b.style.display ="block";
+    }
+    else{
+      b.style.display = "none";
+    }
+  }
+  function clos(){
+    var c=document.getElementById("men");
+    if(c.style.display=="none"){
+      c.style.display ="block";
+    }
+    else{
+      c.style.display = "none";
+    }
+  }
+  
+function hell() {
+  var z = document.getElementById("hell");
+  if(z.style.display=="none"){
+    z.style.display = "block";
+  }
+  else {
+    z.style.display = "none";
+  }
+}
+function he(){
+  var w=document.getElementById("he");
+  if(w.style.display=="none"){
+    w.style.display ="block";
+  }
+  else{
+    w.style.display = "none";
+  }
+}
